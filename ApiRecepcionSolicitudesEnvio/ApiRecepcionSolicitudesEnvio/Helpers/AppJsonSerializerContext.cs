3c5faf2f64725bfcdc949083424dcd11bbc5b5a9
Benjamín <benjamin.diaze@outlook.com>
2025-08-22 00:03:20 -0400

    Se añaden apigatewayproxy request y response jsonserializer

--- ApiRecepcionSolicitudesEnvio/ApiRecepcionSolicitudesEnvio/Helpers/AppJsonSerializerContext.cs
+++ ApiRecepcionSolicitudesEnvio/ApiRecepcionSolicitudesEnvio/Helpers/AppJsonSerializerContext.cs
@@ -1,10 +1,13 @@
-﻿using ApiRecepcionSolicitudesEnvio.Models;
+﻿using Amazon.Lambda.APIGatewayEvents;
+using ApiRecepcionSolicitudesEnvio.Models;
 using System.Text.Json.Serialization;
 
 namespace ApiRecepcionSolicitudesEnvio.Helpers {
 
     [JsonSerializable(typeof(Correo))]
     [JsonSerializable(typeof(DireccionCorreo))]
+    [JsonSerializable(typeof(APIGatewayProxyRequest))]
+    [JsonSerializable(typeof(APIGatewayProxyResponse))]
     internal partial class AppJsonSerializerContext : JsonSerializerContext {
 
     }
