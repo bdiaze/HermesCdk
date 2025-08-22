a0ddb7340d6b7099e127149a8aaedf0a2eed7679
Benjamín <benjamin.diaze@outlook.com>
2025-08-21 20:44:09 -0400

    Initial commit

---
+++ src/BDiazECorreosMasivosCdk/BDiazECorreosMasivosCdkStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace BDiazECorreosMasivosCdk
+{
+    public class BDiazECorreosMasivosCdkStack : Stack
+    {
+        internal BDiazECorreosMasivosCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
