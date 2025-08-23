33b13436d0a890579abb7fda3fc59cad4ae1dfb8
Benjamín <benjamin.diaze@outlook.com>
2025-08-22 22:15:49 -0400

    Se añade autenticación en API

--- ApiRecepcionSolicitudesEnvio/ApiRecepcionSolicitudesEnvio/Program.cs
+++ ApiRecepcionSolicitudesEnvio/ApiRecepcionSolicitudesEnvio/Program.cs
@@ -4,7 +4,10 @@ using Amazon.SQS;
 using Amazon.SQS.Model;
 using ApiRecepcionSolicitudesEnvio.Helpers;
 using ApiRecepcionSolicitudesEnvio.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using System.Diagnostics;
+using System.Security.Claims;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -17,30 +20,52 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi, new SourceGeneratorLambdaJsonSerializer<AppJsonSerializerContext>());
 
+builder.Services.AddSingleton<IAmazonSQS, AmazonSQSClient>();
+builder.Services.AddSingleton<VariableEntorno, VariableEntorno>();
+
+VariableEntorno variableEntorno = new(builder.Environment, builder.Configuration);
+string cognitoBaseUrl = ParameterStoreHelper.ObtenerParametro(variableEntorno.Obtener("PARAMETER_ARN_COGNITO_BASE_URL")).Result;
+string cognitoUserPoolId = ParameterStoreHelper.ObtenerParametro(variableEntorno.Obtener("PARAMETER_ARN_COGNITO_USER_POOL_ID")).Result;
+string cognitoUserPoolClientId = ParameterStoreHelper.ObtenerParametro(variableEntorno.Obtener("PARAMETER_ARN_COGNITO_USER_POOL_CLIENT_ID")).Result;
+string cognitoRegion = ParameterStoreHelper.ObtenerParametro(variableEntorno.Obtener("PARAMETER_ARN_COGNITO_REGION")).Result;
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options => {
+        options.Authority = cognitoBaseUrl;
+        options.MetadataAddress = $"https://cognito-idp.{cognitoRegion}.amazonaws.com/{cognitoUserPoolId}/.well-known/openid-configuration";
+        options.TokenValidationParameters = new TokenValidationParameters {
+            ValidateIssuer = true,
+            ValidIssuer = cognitoBaseUrl,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            NameClaimType = ClaimTypes.NameIdentifier,
+        };
+    });
+builder.Services.AddAuthorization(options => {
+    options.AddPolicy("RequiereScopeEnviarCorreo", policy => policy.RequireClaim("scope", "api/enviar_correo"));
+});
+
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
-string parameterArnSqsQueueUrl;
-if (builder.Environment.IsDevelopment()) {
-    parameterArnSqsQueueUrl = builder.Configuration["VariableEntorno:PARAMETER_ARN_SQS_QUEUE_URL"] ?? throw new Exception("Debes agregar el atributo VariableEntorno > PARAMETER_ARN_SQS_QUEUE_URL en el archivo appsettings.Development.json para ejecutar localmente.");
-} else {
-    parameterArnSqsQueueUrl = Environment.GetEnvironmentVariable("PARAMETER_ARN_SQS_QUEUE_URL") ?? throw new Exception("No se ha configurado la variable de entorno PARAMETER_ARN_SQS_QUEUE_URL.");
-}
-
-string sqsQueueUrl = ParameterStoreHelper.ObtenerParametro(parameterArnSqsQueueUrl).Result;
-
-AmazonSQSClient sqsClient = new();
+builder.Configuration["SQS_QUEUE_URL"] = ParameterStoreHelper.ObtenerParametro(variableEntorno.Obtener("PARAMETER_ARN_SQS_QUEUE_URL")).Result;
 
 var correoApi = app.MapGroup("/Correo");
-correoApi.MapPost("/Enviar", async (Correo correo) => {
+correoApi.MapPost("/Enviar", async (Correo correo, IAmazonSQS sqsClient, IConfiguration config, ClaimsPrincipal user) => {
     Stopwatch stopwatch = Stopwatch.StartNew();
 
     try {
         string jsonCorreo = JsonSerializer.Serialize(correo, typeof(Correo), AppJsonSerializerContext.Default);
 
         SendMessageRequest request = new() {
-            QueueUrl = sqsQueueUrl,
+            MessageGroupId = user.Identity!.Name,
+            QueueUrl = config["SQS_QUEUE_URL"],
             MessageBody = jsonCorreo
         };
 
@@ -60,6 +85,6 @@ correoApi.MapPost("/Enviar", async (Correo correo) => {
 
         return Results.Problem("Ocurrió un error al procesar su solicitud de envío de correo.");
     }
-});
+}).RequireAuthorization("RequiereScopeEnviarCorreo");
 
 app.Run();
