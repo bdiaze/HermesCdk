73684d60a1f2af17a92a5dd9a35eb359b4bc4b59
Benjamín <benjamin.diaze@outlook.com>
2025-09-03 23:53:47 -0400

    Se añade problemdetails a appjsonserializercontext

--- ApiRecepcionSolicitudesEnvio/ApiRecepcionSolicitudesEnvio/Helpers/AppJsonSerializerContext.cs
+++ ApiRecepcionSolicitudesEnvio/ApiRecepcionSolicitudesEnvio/Helpers/AppJsonSerializerContext.cs
@@ -1,15 +1,17 @@
 ﻿using Amazon.Lambda.APIGatewayEvents;
 using ApiRecepcionSolicitudesEnvio.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Text.Json.Serialization;
 
 namespace ApiRecepcionSolicitudesEnvio.Helpers {
 
+    [JsonSerializable(typeof(APIGatewayProxyRequest))]
+    [JsonSerializable(typeof(APIGatewayProxyResponse))]
+    [JsonSerializable(typeof(ProblemDetails))]
     [JsonSerializable(typeof(Correo))]
     [JsonSerializable(typeof(DireccionCorreo))]
     [JsonSerializable(typeof(Adjunto))]
     [JsonSerializable(typeof(Retorno))]
-    [JsonSerializable(typeof(APIGatewayProxyRequest))]
-    [JsonSerializable(typeof(APIGatewayProxyResponse))]
     internal partial class AppJsonSerializerContext : JsonSerializerContext {
 
     }
