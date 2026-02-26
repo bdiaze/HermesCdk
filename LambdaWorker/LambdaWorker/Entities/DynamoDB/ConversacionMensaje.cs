be539b928603159289bdc099c7ad53f9a9edf492
Benjamín <benjamin.diaze@outlook.com>
2026-02-26 19:26:21 -0300

    Se crea modelo de Dynamo para entidades de la tabla conversaciones.

---
+++ LambdaWorker/LambdaWorker/Entities/DynamoDB/ConversacionMensaje.cs
@@ -0,0 +1,75 @@
+﻿using Amazon.DynamoDBv2.Model;
+using LambdaWorker.Enums.DynamoDB;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaWorker.Entities.DynamoDB {
+	internal class ConversacionMensaje : Base {
+		public required string TenantId { get; set; }
+		public required string NumeroTelefono { get; set; }
+		public required string WhatsappMessageId { get; set; }
+		public DireccionMensaje Direccion { get; set; }
+		public TipoMensaje Tipo {  get; set; }
+		public string? Cuerpo { get; set; }
+		public string? NombreTemplate { get; set; }
+		public EstadoMensaje Estado { get; set; }
+		public DateTime FechaCreacion { get; set; }
+		public string? RawPayload { get; set; }
+
+		public override string PK => $"TENANT#{TenantId}#CONVERSACION#{NumeroTelefono}";
+		public override string SK => $"MENSAJE#{FechaCreacion.ToString("o", CultureInfo.InvariantCulture)}#{WhatsappMessageId}";
+
+		public override string? GSI1PK => null;
+		public override string? GSI1SK => null;
+
+		public override Dictionary<string, AttributeValue> ToItem() {
+			Dictionary<string, AttributeValue> item = this.Key.Concat(this.GSI1Attributes).Concat(
+				new Dictionary<string, AttributeValue>() {
+					{ "TenantId", new AttributeValue { S = $"{TenantId}" } },
+					{ "NumeroTelefono", new AttributeValue { S = $"{NumeroTelefono}" } },
+					{ "WhatsappMessageId", new AttributeValue { S = $"{WhatsappMessageId}" } },
+					{ "Direccion", new AttributeValue { S = $"{Direccion}" } },
+					{ "Tipo", new AttributeValue { S = $"{Tipo}" } },
+					{ "Cuerpo", new AttributeValue { NULL = true } },
+					{ "NombreTemplate", new AttributeValue { NULL = true } },
+					{ "Estado", new AttributeValue { S = $"{Estado}" } },
+					{ "FechaCreacion", new AttributeValue { S = $"{FechaCreacion.ToString("o", CultureInfo.InvariantCulture)}" } },
+					{ "RawPayload", new AttributeValue { NULL = true } },
+				}
+			).ToDictionary();
+
+			if (Cuerpo != null) {
+				item["Cuerpo"] = new AttributeValue { S = $"{Cuerpo}" };
+			}
+
+			if (NombreTemplate != null) {
+				item["NombreTemplate"] = new AttributeValue { S = $"{NombreTemplate}" };
+			}
+
+			if (RawPayload != null) {
+				item["RawPayload"] = new AttributeValue { S = $"{RawPayload}" };
+			}
+
+			return item;
+		}
+
+		public static ConversacionMensaje FromItem(Dictionary<string, AttributeValue> item) {
+			return new ConversacionMensaje() {
+				TenantId = item["TenantId"].S,
+				NumeroTelefono = item["NumeroTelefono"].S,
+				WhatsappMessageId = item["WhatsappMessageId"].S,
+				Direccion = Enum.Parse<DireccionMensaje>(item["Direccion"].S),
+				Tipo = Enum.Parse<TipoMensaje>(item["Tipo"].S),
+				Cuerpo = item["Cuerpo"].S,
+				NombreTemplate = item["NombreTemplate"].S,
+				Estado = Enum.Parse<EstadoMensaje>(item["Estado"].S),
+				FechaCreacion = DateTime.ParseExact(item["FechaCreacion"].S, "o", CultureInfo.InvariantCulture),
+				RawPayload = item["RawPayload"].S
+			};
+		}
+	}
+}
---
+++ LambdaWorker/LambdaWorker/Entities/DynamoDB/ConversacionMetadata.cs
@@ -0,0 +1,71 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.SimpleEmailV2.Model;
+using LambdaWorker.Enums.DynamoDB;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaWorker.Entities.DynamoDB {
+	internal class ConversacionMetadata : Base {
+		public required string TenantId { get; set; }
+		public required string NumeroTelefono { get; set; }
+		public DateTime FechaUltimoMensaje { get; set; }
+		public string? PreviewUltimoMensaje { get; set; }
+		public int CantidadNoLeidos { get; set; }
+		public EstadoConversacion Estado { get; set; }
+		public DateTime? FechaUltimaEntrada { get; set; }
+		public DateTime? PuedeResponderGratuitoHasta { get; set; }
+
+		public override string PK => $"TENANT#{TenantId}#CONVERSACION#{NumeroTelefono}";
+		public override string SK => $"METADATA";
+
+		public override string? GSI1PK => $"TENANT#{TenantId}";
+		public override string? GSI1SK => $"{FechaUltimoMensaje.ToString("o", CultureInfo.InvariantCulture)}";
+
+		public override Dictionary<string, AttributeValue> ToItem() {
+			Dictionary<string, AttributeValue> item = this.Key.Concat(this.GSI1Attributes).Concat(
+				new Dictionary<string, AttributeValue>() {
+					{ "TenantId", new AttributeValue { S = $"{TenantId}" } },
+					{ "NumeroTelefono", new AttributeValue { S = $"{NumeroTelefono}" } },
+					{ "FechaUltimoMensaje",new AttributeValue { S = $"{FechaUltimoMensaje.ToString("o", CultureInfo.InvariantCulture)}" } },
+					{ "PreviewUltimoMensaje", new AttributeValue { NULL = true } },
+					{ "CantidadNoLeidos", new AttributeValue { N = $"{CantidadNoLeidos.ToString(CultureInfo.InvariantCulture)}" } },
+					{ "Estado", new AttributeValue { S = $"{Estado}" } },
+					{ "FechaUltimaEntrada", new AttributeValue { NULL = true } },
+					{ "PuedeResponderGratuitoHasta", new AttributeValue { NULL = true } },
+				}
+			).ToDictionary();
+
+			if (PreviewUltimoMensaje != null) {
+				item["PreviewUltimoMensaje"] = new AttributeValue { S = $"{PreviewUltimoMensaje}" };
+			}
+
+			if (FechaUltimaEntrada != null) {
+				item["FechaUltimaEntrada"] = new AttributeValue { S = $"{FechaUltimaEntrada.Value.ToString("o", CultureInfo.InvariantCulture)}" };
+			}
+
+			if (PuedeResponderGratuitoHasta != null) {
+				item["PuedeResponderGratuitoHasta"] = new AttributeValue { S = $"{PuedeResponderGratuitoHasta.Value.ToString("o", CultureInfo.InvariantCulture)}" };
+			}
+
+			return item;
+		}
+
+		public static ConversacionMetadata FromItem(Dictionary<string, AttributeValue> item) {
+			return new ConversacionMetadata() {
+				TenantId = item["TenantId"].S,
+				NumeroTelefono = item["NumeroTelefono"].S,
+				FechaUltimoMensaje = DateTime.ParseExact(item["FechaUltimoMensaje"].S, "o", CultureInfo.InvariantCulture),
+				PreviewUltimoMensaje = item["PreviewUltimoMensaje"].S,
+				CantidadNoLeidos = int.Parse(item["CantidadNoLeidos"].N, CultureInfo.InvariantCulture),
+				Estado = Enum.Parse<EstadoConversacion>(item["Estado"].S),
+				FechaUltimaEntrada = item["FechaUltimaEntrada"].S == null ? null : DateTime.ParseExact(item["FechaUltimaEntrada"].S, "o", CultureInfo.InvariantCulture),
+				PuedeResponderGratuitoHasta = item["PuedeResponderGratuitoHasta"].S == null ? null : DateTime.ParseExact(item["PuedeResponderGratuitoHasta"].S, "o", CultureInfo.InvariantCulture)
+			};
+		}
+	}
+}
