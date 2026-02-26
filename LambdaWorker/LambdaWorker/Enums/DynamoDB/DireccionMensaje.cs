364953ece4389631c43ce0707c696b3f8d73acf0
Benjamín <benjamin.diaze@outlook.com>
2026-02-26 19:27:22 -0300

    Se crean enums para entidades de conversaciones.

---
+++ LambdaWorker/LambdaWorker/Enums/DynamoDB/DireccionMensaje.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaWorker.Enums.DynamoDB {
+	internal enum DireccionMensaje {
+		Entrada = 1,
+		Salida = 2
+	}
+}
---
+++ LambdaWorker/LambdaWorker/Enums/DynamoDB/EstadoMensaje.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaWorker.Enums.DynamoDB {
+	internal enum EstadoMensaje {
+		Recibido = 1,
+		Enviado = 2,
+		Entregado = 3,
+		Leido = 4,
+		Fallido = 5
+	}
+}
